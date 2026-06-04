f193464387d5a73ebb850a947b5449d3901f49c3
LykhotaIvan <ivan.lykhota@student.nhlstenden.com>
2026-06-04 17:25:55 +0200

    test

--- api/config.py
+++ api/config.py
@@ -1,14 +1,15 @@
 import os
 
-SCRAPER_SERVICE_URL = os.getenv("SCRAPER_SERVICE_URL", "http://localhost:5000")
-OLD_LISTINGS_DAYS = int(os.getenv("OLD_LISTINGS_DAYS", "30"))
-SCHEDULER_INTERVAL_MINUTES = int(os.getenv("SCHEDULER_INTERVAL_MINUTES", "10"))
+SCRAPER_SERVICE_URL = os.environ["SCRAPER_SERVICE_URL"]
+OLD_LISTINGS_DAYS = int(os.environ["OLD_LISTINGS_DAYS"])
+SCHEDULER_INTERVAL_MINUTES = int(os.environ["SCHEDULER_INTERVAL_MINUTES"])
 
-MAIL_SERVER = os.getenv("SMTP_HOST", "smtp.gmail.com")
-MAIL_PORT = int(os.getenv("SMTP_PORT", "587"))
-MAIL_USERNAME = os.getenv("SMTP_USER")
-MAIL_PASSWORD = os.getenv("SMTP_PASSWORD")
+MAIL_SERVER = os.environ["SMTP_HOST"]
+MAIL_PORT = int(os.environ["SMTP_PORT"])
+MAIL_USERNAME = os.environ["SMTP_USER"]
+MAIL_PASSWORD = os.environ["SMTP_PASSWORD"]
+
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 
-EMAIL_FROM = os.getenv("EMAIL_FROM", MAIL_USERNAME)
+EMAIL_FROM = os.environ["EMAIL_FROM"]
